25bb7e259a188b849b24ed3ef6d185eb43d3cb13
logic <logic@xaker.ru>
2022-11-14 18:22:03 +0400

    tz up

--- yii_demo/config/web.php
+++ yii_demo/config/web.php
@@ -5,7 +5,6 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'language'=>'ru',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
@@ -15,23 +14,33 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Tp7yfrtkHf-5FBkVeOGOcORWKlf9mxFQ',
-            'baseUrl' => ''
+            'cookieValidationKey' => 'k3wUEV83VnszvjN9HbR-LeCXWzmnNV6n',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'user' => [
+
+        
+/*        'user' => [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
+        ],*/
+
+        'user' => [
+            'class' => 'webvimark\modules\UserManagement\components\UserConfig',
+            'on afterLogin' => function($event) {
+                    \webvimark\modules\UserManagement\models\UserVisitLog::newVisitor($event->identity->id);
+                }
         ],
+
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
         'mailer' => [
-            'class' => \yii\symfonymailer\Mailer::class,
-            'viewPath' => '@app/mail',
-            // send all mails to a file by default.
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
         'log' => [
@@ -44,52 +53,57 @@ $config = [
             ],
         ],
         'db' => $db,
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'enableStrictParsing' => false,
             'showScriptName' => false,
             'rules' => [
-                '' => 'site/index',
-                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
+                 '' => 'patientss/index',
             ],
         ],
         
     ],
+    'modules'=>[
+        'user-management' => [
+            'class' => 'webvimark\modules\UserManagement\UserManagementModule',
+            // 'enableRegistration' => true,
+            // Add regexp validation to passwords. Default pattern does not restrict user and can enter any set of characters.
+            // The example below allows user to enter :
+            // any set of characters
+            // (?=\S{8,}): of at least length 8
+            // (?=\S*[a-z]): containing at least one lowercase letter
+            // (?=\S*[A-Z]): and at least one uppercase letter
+            // (?=\S*[\d]): and at least one number
+            // $: anchored to the end of the string
+            //'passwordRegexp' => '^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$',
+            // Here you can set your handler to change layout for any controller or action
+            // Tip: you can use this event in any module
+            'on beforeAction'=>function(yii\base\ActionEvent $event) {
+                    if ( $event->action->uniqueId == 'user-management/auth/login' )
+                    {
+                        $event->action->controller->layout = 'loginLayout.php';
+                    };
+                },
+        ],
+    ],    
     'params' => $params,
 ];
 
-if (YII_ENV_DEV) {
+//if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-//    $config['bootstrap'][] = 'debug';
-//    $config['modules']['debug'] = [
-//        'class' => 'yii\debug\Module',
-//        // uncomment the following to add your IP if you are not connecting from localhost.
-//        //'allowedIPs' => ['127.0.0.1', '::1'],
-//    ];
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        'allowedIPs' => ['*'],
-//        'traceLine' => '<a href="phpstorm://open?url={file}&line={line}">{file}:{line}</a>',
-        'panels' => [
-            'db' => [
-                'class' => 'yii\debug\panels\DbPanel',
-                'defaultOrder' => [
-                    'seq' => SORT_ASC
-                ],
-                'defaultFilter' => [
-                    'type' => 'SELECT'
-                ]
-            ],
-        ],
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
-    
+
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
-        'class' => \yii\gii\Module::className(),
+        'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['*'],
+        //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
-}
+//}
 
 return $config;
--- yii_demo/controllers/SiteController.php
+++ yii_demo/controllers/SiteController.php
@@ -9,56 +9,16 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
-use app\models\Category;
 
-class SiteController extends Controller {
-
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors() {
-        return [
-            'access' => [
-                'class' => AccessControl::class,
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::class,
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function actions() {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
-
+class SiteController extends BaseController
+{
     /**
      * Displays homepage.
      *
      * @return string
      */
-    public function actionIndex() {
+    public function actionIndex()
+    {
         return $this->render('index');
     }
 
@@ -67,7 +27,8 @@ class SiteController extends Controller {
      *
      * @return Response|string
      */
-    public function actionLogin() {
+    public function actionLogin()
+    {
         if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
@@ -79,7 +40,7 @@ class SiteController extends Controller {
 
         $model->password = '';
         return $this->render('login', [
-                    'model' => $model,
+            'model' => $model,
         ]);
     }
 
@@ -88,7 +49,8 @@ class SiteController extends Controller {
      *
      * @return Response
      */
-    public function actionLogout() {
+    public function actionLogout()
+    {
         Yii::$app->user->logout();
 
         return $this->goHome();
@@ -99,7 +61,8 @@ class SiteController extends Controller {
      *
      * @return Response|string
      */
-    public function actionContact() {
+    public function actionContact()
+    {
         $model = new ContactForm();
         if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
             Yii::$app->session->setFlash('contactFormSubmitted');
@@ -107,7 +70,7 @@ class SiteController extends Controller {
             return $this->refresh();
         }
         return $this->render('contact', [
-                    'model' => $model,
+            'model' => $model,
         ]);
     }
 
@@ -116,47 +79,8 @@ class SiteController extends Controller {
      *
      * @return string
      */
-    public function actionAbout() {
+    public function actionAbout()
+    {
         return $this->render('about');
     }
-
-    public function actionCategory() {
-        $id = Yii::$app->request->get('category_id');
-        $cats = [];
-        $crumbs = [];
-        $firstSort = false;
-        if (!$id) {
-            $cats = $this->getParentCat();
-            $firstSort = false;
-        } else {
-            $cats = Category::find()->with(['children', 'parent'])->where(['id' => $id])->all();
-            $crumbs = $this->generateBreadcrumbs($cats[0]);
-            $cats = $cats[0]->children;
-            $firstSort = true;
-        }
-
-        return $this->render('category', compact('cats', 'crumbs', 'firstSort'));
-    }
-
-    private function getParentCat() {
-        $catsCash = yii::$app->cache->get('parentCategory');
-        if ($catsCash) {
-            return $catsCash;
-        }
-        $cats = Category::find()->with(['children', 'parent'])->where(['parent_id' => null])->all();
-        yii::$app->cache->set('parentCategory', $cats, 60 * 60);
-        return $cats;
-    }
-
-    private function generateBreadcrumbs($cats) {
-        if (isset($cats->parent)) {
-            $curArr = [];
-            $curArr = array_merge($curArr, $this->generateBreadcrumbs($cats->parent));
-            $curArr = array_merge($curArr, [['label' => $cats->name, 'url' => $cats->url]]);
-            return $curArr;
-        } else {
-            return [['label' => $cats->name, 'url' => $cats->url]];
-        }
-    }
-
 }
